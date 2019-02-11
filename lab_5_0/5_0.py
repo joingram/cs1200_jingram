36c88034afbbd2c00d432367580734fdc3448f4c
Neea Rusch <nrusch@augusta.edu>
2019-02-11 14:44:51 -0500

    added lab exercise 5.0

---
+++ lab_5_0/5_0.py
@@ -0,0 +1,215 @@
+"""
+CSCI 1200
+Lab Exercise 5.0
+
+IF STATEMENTS
+
+In this lab we will learn how to run code based on some conditions.
+Do all work in Geany IDE and follow instructions provided in this 
+code file.
+
+Learning Outcomes
+=================
+In this lab you will learn how to:
+- write conditional statements
+- declare if statements
+- declare if-else statements
+- declare if-elif-else statements 
+
+
+Evaluation
+==========
+20 pts. - submitted filename 5_0.py
+20 pts. - 5_0.py executes without error
+30 pts. - Part 1 meets requirements
+30 pts. - Part 2 meets requirements
+
+
+Submission
+==========
+As before, submit your work on D2L or using git.
+Earn 5 pts. extra credit by submitting using git by due date.
+
+"""
+
+#=======================================================================
+# PART 1
+#-----------------------------------------------------------------------
+#
+# In this part we will practice how to use conditional operators.
+# We will practice how to use many different operators so you will
+# get a good understanding of how they work.
+#
+# In this part when you are asked to print a conditional statement,
+# you should do something similar to this:
+#
+# print(your_conditional_statement_here)
+#
+#-----------------------------------------------------------------------
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print('Part 1A ' + ('='*20))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+# 1a) You are provided following list of colors:
+
+colors = ['blue', 'blue', 'green']
+
+
+# Choose two colors from the list that are equal. Prove that they are 
+# in fact equal by printing a conditional statement where you test
+# their equality.
+
+
+
+
+
+# Choose two colors that are not equal. Prove that they are not equal
+# by printing a conditional statement where you test their inequality.
+
+
+
+
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print('\nPart 1B ' + ('='*20))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+# 1b) You are provided following ages:
+
+user1_age = 23
+user2_age = 20
+user3_age = 29
+user4_age = 21
+user5_age = 27
+
+#  Is user1 older than user5? Print your conditional statement.
+
+
+
+
+#  Is user3 younger than user2? Print your conditional statement.
+
+
+
+
+#  Is user4 21 or older? Print your conditional statement.
+
+
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print('\nPart 1C ' + ('='*20))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+# 1c) Declare a list variable called states which contains following 
+#      values: Georgia, Texas, Oregon, Arizona, Maine
+
+
+
+# Arizona is in the list of states. Print a conditonal statement where
+# you prove that this is True.
+
+
+
+
+# Ohio is not in the list. Print a conditional statement where you
+# prove that this is True. 
+
+
+
+
+
+
+#=======================================================================
+# PART 2
+#-----------------------------------------------------------------------
+#
+# In this part we will practice how to use if statements.
+# We will practice several different cases so you can learn how they
+# behave differently and learn how to choose the correct statement
+# based on the requirements for your code.
+#
+#-----------------------------------------------------------------------
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print('\n\nPart 2A ' + ('='*20))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+# 2a) Declare a boolean varible is_student. You can set the value
+# to by any boolean value.
+
+
+
+
+
+# If user is a student, print "Hello student!". 
+# Write this logical expression in python below. 
+
+
+
+
+
+
+# TIP: You should change value of is_student and execute your code again
+# to verify that your statement works correctly
+
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print('\nPart 2B ' + ('='*20))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+# 2b) Declare string variable called country. Assing the value
+# to be any country you want
+
+
+
+
+# if country is 'United States' print 'USA'. Else print the
+# variable value as-is.
+
+
+
+
+
+# TIP: You should change value of country and execute your code again
+# to verify that your statement works correctly
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+print('\nPart 2C ' + ('='*20))
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+# 2c) Declare integer variable exam_grade. 
+
+
+
+
+# Implement following logic in python:
+#
+# if exam grade is greater or equal to 90, 
+# print 'Your grade is A! Good Job!'
+# 
+# else if exam grade is greater or equal to 80, 
+# print 'Your grade is B'
+#
+# else if exam grade is greater or equal to 70,
+# print 'Your grade is C'
+# 
+# else if exam grade is greater or equal to 60, 
+# print 'Your grade is D'
+#
+# else print 'You failed'
+
+
+
+
+
+
+
+# TIP: You should change value of exam_grade and execute your code again
+# to verify that your statement works correctly
+
+#=======================================================================
+# That's it for today!
+# End of Lab Exercise 5.0
+#=======================================================================
