f0e2103a0cbf2f24637da13e6b130d579e158fc0
Neea Rusch <nrusch@augusta.edu>
2019-03-11 15:09:47 -0400

    added lab exercise 8.0

---
+++ lab_8_0/8_0.py
@@ -0,0 +1,89 @@
+"""
+CSCI 1200
+Lab Exercise 8.0
+
+FUNCTIONS
+
+In this lab we will learn how to create functions. Focus on writing 
+good functions and calling them correctly. Do all work in Geany IDE and 
+follow instructions provided in this code file.
+
+Learning Outcomes
+=================
+In this lab you will learn how to:
+- declare functions
+- specify required parameters
+- return values
+- call functions
+
+Evaluation
+==========
+20 pts. - submitted filename 8_0.py
+20 pts. - 8_0.py executes without error
+30 pts. - Part 1 meets requirements
+30 pts. - Part 2 meets requirements
+
+Submission
+==========
+As before, submit your work on D2L or using git.
+Earn 5 pts. extra credit by submitting using git by due date.
+
+"""
+
+
+#=======================================================================
+# PART 1
+#-----------------------------------------------------------------------
+# In this part you will create math functions that return values.
+#
+# Implement functions to perform addition, subtraction, multiplication, 
+# division and the remainder (modulo) operation on the two numbers. 
+# Write each function to accept the two integers as parameters and 
+# to return an integer. Don't forget the docstrings!
+#-----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+#=======================================================================
+# PART 2
+#-----------------------------------------------------------------------
+# In this part you will write an input program where you perform 
+# the following steps:
+# 
+# 1. Welcome the user to Lab Exercise 8.0
+#
+# 2. Prompt for the user's name. Receive the name into the program and 
+# save it as the value of a name variable.
+#
+# 3. Prompt the user two integers. Receive the numbers and save them as 
+# variable values. 
+#
+# 4. Make function calls to perform the calculations you created in 
+# part 1. Ensure you send both numbers as arguments to the functions. 
+# Write the results of  the function calls out to the screen. 
+#
+# See sample output here: https://i.imgur.com/l3xEoJP.png
+#-----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+#=======================================================================
+# That's it for today!
+# End of Lab Exercise 8.0
+#=======================================================================
