20545392fa93747eaf3bfcac5991168c02523ec6
Joingram <joingram@augusta.edu>
2019-03-20 18:34:26 -0400

    Lab9.1

--- lab_9_1/9_1.py
+++ lab_9_1/9_1.py
@@ -60,7 +60,40 @@ Earn 5 pts. extra credit by submitting using git by due date.
 #    tank filled". This completes your Car class.
 #-----------------------------------------------------------------------
 
-
+class Car():
+	"""class is car"""
+	def _init_(self, make, model):
+		self.make = make
+		self.model = model
+		self.year = year
+		self.mileage_reading = 0
+		
+	def get_descriptive_name(self):
+		long_name = str(self.year) + ' ' + self.make + ' ' + slef.model
+		return long_name.title()
+		
+	def read_mileage(self):
+		print("This car has" + str(self.mileage_reading) + " miles on it.")
+		
+	def update_odomoter(self, mileage):
+		if mileage >= self.mileage_reading:
+			self.mileage_reading = mileage
+		else:
+			print("You can't roll back on mileage.")
+			
+		def increment_mileage(self, miles):
+			self.mileage_reader += miles 
+		
+class ElectronicCar(Car):
+	"""Represent aspects of a car , specific to electric vehicles."""
+	def _init_(self, make, model, year):
+		"""Initialize attributes of the parent class."""
+		super()._init_(make, model, year)
+		
+		
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+		
 
 
 
