1f3e8b79d30a045c13852cb7cfee09e51c4bb1df
Joingram <joingram@augusta.edu>
2019-03-27 16:25:46 -0400

    Lab 9

---
+++ 9_0.py
@@ -0,0 +1,93 @@
+"""
+CSCI 1200
+Lab Exercise 9.0
+
+CLASSES
+
+In this lab you will write your first python class. Do all work in 
+Geany IDE and follow instructions provided in this code file.
+
+Learning Outcomes
+=================
+In this lab you will learn how to:
+- Declare classes
+- Declare and use attributes
+- Declare and use methods
+- Instantiate objects
+
+Evaluation
+==========
+20 pts. - submitted filename 9_0.py
+20 pts. - 9_0.py executes without error
+40 pts. - User class definition meets requirements
+20 pts. - User objects instantiated and used correctly
+
+Submission
+==========
+As before, submit your work on D2L or using git.
+Earn 5 pts. extra credit by submitting using git by due date.
+
+"""
+
+#=======================================================================
+# ASSIGNMENT INSTRUCTIONS
+#-----------------------------------------------------------------------
+# In this part you will learn how to declare a python class. Remember 
+# a class describes a real-world entity and contains attributes and 
+# methods. Your mission in this lab exercise is to make a class called 
+# User. This User class will hold the user info commonly associated 
+# with an online login.
+#
+# STEPS:
+# 
+# 1. Declare a class called User.
+# 
+# 2. Within User class, create attributes for the username, first name 
+#    and last name.
+# 
+# 3. Within User class, make a method called describe_user() that prints 
+#    a summary of the user's information.
+#
+# 4. Within User class, make another method called greet_user() that 
+#    prints a personalized greeting to the user.
+#
+# 5. Create three instances representing different users, and call both 
+#    methods for each user.
+#-----------------------------------------------------------------------
+
+class User():
+    """class is user"""
+    def __init__(self, first, last, username):
+        """defining first and last name"""
+        self.first = first
+        self.last = last
+        self.username = username
+        
+    def describe_user(self):
+        print('first name' + self.first + " last name = " + self.last + " Username " + self.username)
+        
+    def greet_user(self):
+        print("Hello, " + self.first.title() + ", welcome.")
+        
+someuser = User('Jon', 'Ingram', 'Chief')
+someuser.describe_user()
+someuser.greet_user()
+
+user2 = User('Josh', 'Gordon', "Josgor")
+user2.describe_user()
+user2.greet_user()
+
+user3 = User('Phillip', 'Engel', "Aorri")
+user3.describe_user()
+user3.greet_user()
+
+
+
+
+
+
+
+#=======================================================================
+# That's it for today!
+# End of Lab Exercise 9.0
+#=======================================================================
