0656ae6f4eaa97a857080ca2b934469f59f6cbe2
Joingram <joingram@augusta.edu>
2019-04-03 19:05:34 -0400

    11.0 revised again

--- lab_11_0/my_functionstest.py
+++ lab_11_0/my_functionstest.py
@@ -4,7 +4,7 @@ import my_functions
 class TestMyFunctions(unittest.TestCase):
 	def test_addnumbers(self):
 		self.assertEqual(my_functions.add_numbers(3,2),5)
-	def test_addnumbers2(self):
+	def test_addnumbers(self):
 		self.assertEqual(my_functions.add_numbers(3,3),6)
 	def test_addnumbers2(self):
 		self.assertEqual(my_functions.add_numbers(3,3),6)
@@ -21,6 +21,8 @@ class TestMyFunctions(unittest.TestCase):
 	def test_is_prime2(self):
 		self.assertEqual(my_functions.is_prime(21), False)
 	def test_letters(self):
+		self.assertEqual(my_functions.letters('Hello World'), [' ', 'H', 'W', 'd', 'e', 'l', 'o', 'r'])
+	def test_letters2(self):
 		self.assertEqual(my_functions.letters('Hello World'), [' ', 'H', 'W', 'd', 'e', 'l', 'o', 'r'])
 	
 		
